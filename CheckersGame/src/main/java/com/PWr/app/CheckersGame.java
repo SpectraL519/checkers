3eecca7dfba56876c09324bbc9885d4ca5ca48a7
Jakub Musiał <jmusial2003@gmail.com>
2022-12-11 14:16:24 +0100

    Init CheckersGame.java, CheckersGameTest.java, pom.xml

---
+++ CheckersGame/src/main/java/com/PWr/app/CheckersGame.java
@@ -0,0 +1,14 @@
+package com.PWr.app;
+
+
+
+
+
+/**
+ * Welcome!
+ */
+public class CheckersGame {
+    public static void main (String[] args) {
+        System.out.println("Welcome to the Checkers Game!");
+    }
+}
---
+++ CheckersGame/src/test/java/com/PWr/app/CheckersGameTest.java
@@ -0,0 +1,19 @@
+package com.PWr.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+
+
+/**
+ * Initial test
+ */
+public class CheckersGameTest {
+    @Test
+    public void shouldAnswerWithTrue () {
+        assertTrue(true);
+    }
+}
