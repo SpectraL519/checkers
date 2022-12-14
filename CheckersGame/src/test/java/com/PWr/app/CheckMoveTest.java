57e1af2ac2d21cef1876e7cdf553699048fee5c1
SpectraL519 <jmusial2003@gmail.com>
2022-12-14 03:43:35 +0100

    Init CheckMoveTest.java

---
+++ CheckersGame/src/test/java/com/PWr/app/CheckMoveTest.java
@@ -0,0 +1,44 @@
+package com.PWr.app;
+
+import com.PWr.app.Model.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+
+
+
+
+
+/**
+ * Initial test
+ */
+public class CheckMoveTest {
+    @Test
+    public void shouldAnswerWithTrue () {
+        GameModel model = new GameModel();
+
+        model.setVersion("Russian");
+        assertThat(model.getVersion(), instanceOf(RussianVersion.class));
+        model.initPawns();
+        model.displayPawns();
+
+        System.out.println();
+
+        System.out.println("Move 1");
+        assertEquals(model.movePawn(5, 2, 4, 1), 1);
+        model.displayPawns();
+
+        System.out.println("Move 2");
+        assertEquals(model.movePawn(4, 1, 3, 0), 1);
+        model.displayPawns();
+
+        System.out.println("Move 3");
+        assertEquals(model.movePawn(3, 0, 2, -1), -1);
+        model.displayPawns();
+    }
+}
