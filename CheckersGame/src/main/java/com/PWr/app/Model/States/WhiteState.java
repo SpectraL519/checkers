3a12e69024c7ed75a59b4978c3e73fc8d37dba62
Jakub Musiał <jmusial2003@gmail.com>
2022-12-17 20:15:07 +0100

    Init States package

---
+++ CheckersGame/src/main/java/com/PWr/app/Model/States/WhiteState.java
@@ -0,0 +1,36 @@
+package com.PWr.app.Model.States;
+
+
+
+
+
+public class WhiteState implements GameStateBahaviour {
+    @Override
+    public GameState getState () {
+        return GameState.WHITE;
+    }
+
+
+
+    @Override
+    public GameStateBahaviour startGame () {
+        // The game has already started
+        return this;
+    }
+
+
+
+    @Override
+    public GameStateBahaviour switchPlayer () {
+        // Switch player to black
+        return GameState.BLACK.getStateBahaviour();
+    }
+
+
+
+    @Override
+    public GameStateBahaviour endGame () {
+        // Switch to resting state
+        return GameState.RESTING.getStateBahaviour();
+    }
+}
