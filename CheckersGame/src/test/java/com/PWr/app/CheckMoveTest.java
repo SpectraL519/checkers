eddcf42df5d71769ad2333b187211241b572d3a9
Jakub Musiał <jmusial2003@gmail.com>
2022-12-17 20:17:19 +0100

    Modified the CheckMoveTest.java unit test

--- CheckersGame/src/test/java/com/PWr/app/CheckMoveTest.java
+++ CheckersGame/src/test/java/com/PWr/app/CheckMoveTest.java
@@ -1,6 +1,7 @@
 package com.PWr.app;
 
 import com.PWr.app.Model.*;
+import com.PWr.app.Model.States.GameState;
 
 import org.junit.Test;
 
@@ -13,10 +14,6 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 
 
 
-
-/**
- * Initial test
- */
 public class CheckMoveTest {
     @Test
     public void shouldAnswerWithTrue () {
@@ -24,24 +21,72 @@ public class CheckMoveTest {
 
         model.setVersion("Russian");
         assertThat(model.getVersion(), instanceOf(RussianVersion.class));
+        assertEquals(model.getState(), GameState.RESTING);
         model.initBoard();
+        assertEquals(model.getState(), GameState.WHITE);
+        model.displayBoard();
+
+        // Trying to move a black pawn first
+        assertEquals(model.movePawn(2, 1, 3, 0), -3);
+
+
+
+        // Trying to move a white pawn incorrectly
+        // Curr not on board
+        assertEquals(model.movePawn(-5, 2, 4, 3), -1);
+        assertEquals(model.movePawn(5, -2, 4, 3), -1);
+
+        // Mov not on board
+        assertEquals(model.movePawn(5, 2, -4, 3), -2);
+        assertEquals(model.movePawn(5, 2, 4, -3), -2);
+
+        // Incorrect player
+        assertEquals(model.movePawn(5, 1, 4, 3), -3);
+
+        // Pawn on Mov
+        assertEquals(model.movePawn(5, 2, 2, 3), -4);
+
+        // Incorrect step ()
+        assertEquals(model.movePawn(5, 2, 4, 2), -5); // Not diagonal movement
+        assertEquals(model.movePawn(5, 2, 3, 4), -5); // Step = 2 without taking an enemy pawn
+
+
+
+        // Moving a white pawn
+        assertEquals(model.movePawn(5, 6, 4, 7), 1);
+        model.displayBoard();
+
+        // Trying to move a white pawn again
+        assertEquals(model.movePawn(4, 3, 3, 4), -3);
+
+
+
+        // Simulating a gameplay
+        assertEquals(model.movePawn(2, 5, 3, 4), 1); // black
+        model.displayBoard();
+
+        assertEquals(model.movePawn(5, 0, 4, 1), 1); // white
+        model.displayBoard();
+
+        // Moving the black pawn backwards
+        assertEquals(model.movePawn(3, 4, 2, 5), -5);
         
-        System.out.println("Mocking a situation");
+        assertEquals(model.movePawn(1, 6, 2, 5), 1); // black
         model.displayBoard();
-        assertEquals(model.movePawn(2, 5, 3, 4), 1);
+
+        // Moving the white pawn backwards
+        assertEquals(model.movePawn(4, 1, 5, 0), -5);
+
+        assertEquals(model.movePawn(6, 5, 5, 6), 1); // white
         model.displayBoard();
-        assertEquals(model.movePawn(3, 4, 4, 3), 1);
+
+        assertEquals(model.movePawn(3, 4, 4, 3), 1); // black
         model.displayBoard();
-        assertEquals(model.movePawn(1, 6, 2, 5), 1);
-        model.displayBoard();
+
+        // White taking 2 black pawns
         assertEquals(model.movePawn(5, 2, 3, 4), 2);
         model.displayBoard();
         assertEquals(model.movePawn(3, 4, 1, 6), 1);
         model.displayBoard();
-
-        System.out.printf("\nBoard after mock:\n");
-        model.displayBoard();
-
-        // Other movement tests
     }
 }
